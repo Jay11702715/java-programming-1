890c1ae5319aecd42dca9c4c93f54410fef1b8ce
Jay11702715 <49591179+Jay11702715@users.noreply.github.com>
2020-08-17 21:36:35 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,22 @@
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    double getRadius() {
+        return radius;
+    }
+    double getArea() {
+        return ((22 * radius * radius)/7);
+    }
+
+    public static void main(String[] args) {
+        Circle obj = new Circle();
+        System.out.println(obj.getRadius());
+        System.out.println(obj.getArea());
+    }
+}
